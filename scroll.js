68f52435618ce6edbf9536088e25fac1edb93e66
Bambang Istijab <bambangistijab30@gmail.com>
2026-02-12 10:48:02 +0700

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const container = document.querySelector(".horizontal-scroll");
 let isDown = false;
 let startX;
@@ -37,3 +38,44 @@ document.addEventListener("keydown", (e) => {
     container.scrollLeft -= 100; // Scroll ke kiri
   }
 });
+=======
+const container = document.querySelector(".horizontal-scroll");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+// Drag Scroll
+container.addEventListener("mousedown", (e) => {
+  isDown = true;
+  container.classList.add("active");
+  startX = e.pageX - container.offsetLeft;
+  scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener("mouseleave", () => {
+  isDown = false;
+  container.classList.remove("active");
+});
+
+container.addEventListener("mouseup", () => {
+  isDown = false;
+  container.classList.remove("active");
+});
+
+container.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 2; // Sesuaikan kecepatan geser
+  container.scrollLeft = scrollLeft - walk;
+});
+
+// Scroll dengan tombol panah
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowRight") {
+    container.scrollLeft += 100; // Scroll ke kanan
+  } else if (e.key === "ArrowLeft") {
+    container.scrollLeft -= 100; // Scroll ke kiri
+  }
+});
+>>>>>>> b8dda5d (smp yuppentek 1 legok)
--- sweetalert.js
+++ sweetalert.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 window.onload = function() {
     Swal.fire({
         // title: 'Brosur Sekolah',
@@ -8,3 +9,15 @@ window.onload = function() {
         showConfirmButton: false 
     });
 };
+=======
+window.onload = function() {
+    Swal.fire({
+        // title: 'Brosur Sekolah',
+        imageUrl: 'assets/brosur.png',
+        imageWidth: 600,
+        imageAlt: 'Brosur Sekolah',
+        showCloseButton: true,
+        showConfirmButton: false 
+    });
+};
+>>>>>>> b8dda5d (smp yuppentek 1 legok)
